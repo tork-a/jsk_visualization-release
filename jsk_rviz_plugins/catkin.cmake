a3f74bcf548d028f1b7259cddfd8f24461a3bd5a
Kei Okada <kei.okada@gmail.com>
2014-07-23 15:09:03 +0900

    Rebase from 'upstream'

--- jsk_rviz_plugins/catkin.cmake
+++ jsk_rviz_plugins/catkin.cmake
@@ -51,7 +51,10 @@ qt4_wrap_cpp(MOC_FILES
   src/plotter_2d_display.h
   src/pie_chart_display.h
   src/diagnostics_display.h
+  src/quiet_interactive_marker_display.h
   src/bounding_box_array_display.h
+  src/overlay_diagnostic_display.h
+  src/sparse_occupancy_grid_array_display.h
 )
 
 set(SOURCE_FILES
@@ -70,6 +73,9 @@ set(SOURCE_FILES
   src/pie_chart_display.cpp
   src/diagnostics_display.cpp
   src/bounding_box_array_display.cpp
+  src/quiet_interactive_marker_display.cpp
+  src/overlay_diagnostic_display.cpp
+  src/sparse_occupancy_grid_array_display.cpp
   ${MOC_FILES}
 )
 
