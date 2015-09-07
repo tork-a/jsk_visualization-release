97835dfacdd5ed1019e5114d5d252848cb80068c
Kei Okada <kei.okada@gmail.com>
2015-09-08 01:20:47 +0900

    Rebase from 'debian/hydro/jsk_rviz_plugins'

--- src/tf_trajectory_display.h
+++ src/tf_trajectory_display.h
@@ -1,8 +1,8 @@
-// -*- mode: c++; -*-
+// -*- mode: c++ -*-
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2014, JSK Lab
+ *  Copyright (c) 2015, JSK Lab
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -32,69 +32,50 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
-#ifndef JSK_RVIZ_PLUGIN_OVERLAY_IMAGE_DISPLAY_H_
-#define JSK_RVIZ_PLUGIN_OVERLAY_IMAGE_DISPLAY_H_
 
-#include <rviz/display.h>
-#include <OGRE/OgreTexture.h>
-#include <OGRE/OgreColourValue.h>
-#include <OGRE/OgreMaterial.h>
+#ifndef JSK_RVIZ_PLUGINS_TF_TRAJECTORY_DISPLAY_H_
+#define JSK_RVIZ_PLUGINS_TF_TRAJECTORY_DISPLAY_H_
 
-#include <QPainter>
-
-#include <rviz/properties/ros_topic_property.h>
-#include <rviz/properties/int_property.h>
+#include <rviz/message_filter_display.h>
 #include <rviz/properties/float_property.h>
-
-#include <image_transport/image_transport.h>
-#include <sensor_msgs/Image.h>
-
-#include "overlay_utils.h"
+#include <rviz/properties/tf_frame_property.h>
+#include <rviz/properties/color_property.h>
+#include <rviz/properties/status_property.h>
+#include <rviz/ogre_helpers/billboard_line.h>
+#include <geometry_msgs/PointStamped.h>
 
 namespace jsk_rviz_plugins
 {
-  class OverlayImageDisplay : public rviz::Display
+
+  class TFTrajectoryDisplay: public rviz::Display
   {
     Q_OBJECT
   public:
-    OverlayImageDisplay();
-    virtual ~OverlayImageDisplay();
-
+    TFTrajectoryDisplay();
+    virtual ~TFTrajectoryDisplay();
   protected:
-    boost::mutex mutex_;
-    OverlayObject::Ptr overlay_;
-    rviz::RosTopicProperty* update_topic_property_;
-    rviz::IntProperty* width_property_;
-    rviz::IntProperty* height_property_;
-    rviz::IntProperty* left_property_;
-    rviz::IntProperty* top_property_;
-    rviz::FloatProperty* alpha_property_;
-    int width_, height_, left_, top_;
-    double alpha_;
-    boost::shared_ptr<image_transport::ImageTransport> it_;
-    image_transport::Subscriber sub_;
-    sensor_msgs::Image::ConstPtr msg_;
-    bool is_msg_available_;
-    bool require_update_;
-
-    virtual void redraw();
     virtual void onInitialize();
     virtual void onEnable();
     virtual void onDisable();
     virtual void update(float wall_dt, float ros_dt);
-    virtual void subscribe();
-    virtual void unsubscribe();
-    virtual void processMessage(const sensor_msgs::Image::ConstPtr& msg);
-  protected Q_SLOTS:
-    void updateTopic();
-    void updateWidth();
-    void updateHeight();
-    void updateLeft();
-    void updateTop();
-    void updateAlpha();
-  };
 
+    rviz::TfFrameProperty* frame_property_;
+    rviz::FloatProperty* duration_property_;
+    rviz::ColorProperty* color_property_;
+    rviz::FloatProperty* line_width_property_;
+    rviz::BillboardLine* line_;
+    std::vector<geometry_msgs::PointStamped> trajectory_;
+    std::string frame_;
+    std::string fixed_frame_;
+    float duration_;
+    QColor color_;
+    float line_width_;
+  protected Q_SLOTS:
+    void updateFrame();
+    void updateDuration();
+    void updateColor();
+    void updateLineWidth();
+  };
 }
 
 #endif
-
