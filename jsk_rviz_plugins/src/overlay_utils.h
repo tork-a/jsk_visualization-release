62540703a2266c27657fd3680be9f13b300a6b9e
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2024-12-11 15:12:15 +0900

    Rebase from 'upstream'

--- jsk_rviz_plugins/src/overlay_utils.h
+++ jsk_rviz_plugins/src/overlay_utils.h
@@ -13,7 +13,7 @@
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
  *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
+ *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
  *   * Neither the name of the JSK Lab nor the names of its
  *     contributors may be used to endorse or promote products derived
@@ -50,6 +50,7 @@
   #include <OGRE/OgreOverlayContainer.h>
   #include <OGRE/OgreOverlayManager.h>
 #else
+  #include <OGRE/Overlay/OgreOverlay.h>
   #include <OGRE/Overlay/OgrePanelOverlayElement.h>
   #include <OGRE/Overlay/OgreOverlayElement.h>
   #include <OGRE/Overlay/OgreOverlayContainer.h>
