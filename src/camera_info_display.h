83df2c14da2fcec4b57e56d0ff3f79c97994da5d
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2024-12-11 15:14:51 +0900

    Rebase from 'debian/noetic/jsk_rviz_plugins'

--- src/camera_info_display.h
+++ src/camera_info_display.h
@@ -13,7 +13,7 @@
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
  *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
+ *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
  *   * Neither the name of the JSK Lab nor the names of its
  *     contributors may be used to endorse or promote products derived
@@ -53,6 +53,7 @@
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreTextureManager.h>
 #include <OGRE/OgreTexture.h>
+#include <OGRE/OgreTechnique.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/subscriber.h>
